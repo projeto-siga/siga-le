b462848c04b0f4a5011083e8c0e3062005b8799f
Renato Crivano <renato.crivano@gmail.com>
2019-02-14 18:15:43 -0200

    Corrigindo problema na geração do PDF que impossibilitava a assinatura com senha

--- src/main/java/br/jus/trf2/siga/ex/api/DocSiglaAssinarComSenhaPost.java
+++ src/main/java/br/jus/trf2/siga/ex/api/DocSiglaAssinarComSenhaPost.java
@@ -9,6 +9,7 @@ import org.hibernate.Hibernate;
 import org.hibernate.proxy.HibernateProxy;
 
 import com.crivano.swaggerservlet.PresentableUnloggedException;
+import com.crivano.swaggerservlet.SwaggerServlet;
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
 import com.google.gson.TypeAdapterFactory;
@@ -22,19 +23,24 @@ import br.gov.jfrj.siga.dp.DpPessoa;
 import br.gov.jfrj.siga.ex.ExDocumento;
 import br.gov.jfrj.siga.ex.ExMobil;
 import br.gov.jfrj.siga.ex.ExPapel;
+import br.gov.jfrj.siga.ex.bl.CurrentRequest;
 import br.gov.jfrj.siga.ex.bl.Ex;
+import br.gov.jfrj.siga.ex.bl.RequestInfo;
 import br.gov.jfrj.siga.persistencia.ExMobilDaoFiltro;
 import br.jus.trf2.siga.ex.api.ISigaDoc.DocSiglaAssinarComSenhaPostRequest;
 import br.jus.trf2.siga.ex.api.ISigaDoc.DocSiglaAssinarComSenhaPostResponse;
 import br.jus.trf2.siga.ex.api.ISigaDoc.IDocSiglaAssinarComSenhaPost;
 import br.jus.trf2.siga.ex.api.TokenCriarPost.Usuario;
 
-public class DocSiglaAssinarComSenhaPost implements
-		IDocSiglaAssinarComSenhaPost {
+public class DocSiglaAssinarComSenhaPost implements IDocSiglaAssinarComSenhaPost {
 
 	@Override
-	public void run(DocSiglaAssinarComSenhaPostRequest req,
-			DocSiglaAssinarComSenhaPostResponse resp) throws Exception {
+	public void run(DocSiglaAssinarComSenhaPostRequest req, DocSiglaAssinarComSenhaPostResponse resp) throws Exception {
+		// Necessário pois é chamado o método "realPath" durante a criação do
+		// PDF.
+		CurrentRequest.set(
+				new RequestInfo(null, SwaggerServlet.getHttpServletRequest(), SwaggerServlet.getHttpServletResponse()));
+
 		String authorization = TokenCriarPost.assertAuthorization();
 		Usuario u = TokenCriarPost.assertUsuario();
 
@@ -51,18 +57,13 @@ public class DocSiglaAssinarComSenhaPost implements
 
 				assertAcesso(mob, titular, lotaTitular);
 
-				if (!Ex.getInstance().getComp()
-						.podeAssinarComSenha(titular, lotaTitular, mob))
-					throw new PresentableUnloggedException("O documento " + req.sigla
-							+ " não pode ser assinado com senha por "
-							+ titular.getSiglaCompleta() + "/"
-							+ lotaTitular.getSiglaCompleta());
+				if (!Ex.getInstance().getComp().podeAssinarComSenha(titular, lotaTitular, mob))
+					throw new PresentableUnloggedException(
+							"O documento " + req.sigla + " não pode ser assinado com senha por "
+									+ titular.getSiglaCompleta() + "/" + lotaTitular.getSiglaCompleta());
 
-				Ex.getInstance()
-						.getBL()
-						.assinarDocumentoComSenha(cadastrante, lotaTitular,
-								doc, null, u.usuario, null, false,
-								titular, false, null, false);
+				Ex.getInstance().getBL().assinarDocumentoComSenha(cadastrante, lotaTitular, doc, null, u.usuario, null,
+						false, titular, false, null, false);
 
 				db.commit();
 				resp.status = "OK";
@@ -72,15 +73,12 @@ public class DocSiglaAssinarComSenhaPost implements
 		}
 	}
 
-	private void assertAcesso(final ExMobil mob, DpPessoa titular,
-			DpLotacao lotaTitular) throws Exception {
-		if (!Ex.getInstance().getComp()
-				.podeAcessarDocumento(titular, lotaTitular, mob)) {
+	private void assertAcesso(final ExMobil mob, DpPessoa titular, DpLotacao lotaTitular) throws Exception {
+		if (!Ex.getInstance().getComp().podeAcessarDocumento(titular, lotaTitular, mob)) {
 			String s = "";
 			s += mob.doc().getListaDeAcessosString();
 			s = "(" + s + ")";
-			s = " " + mob.doc().getExNivelAcessoAtual().getNmNivelAcesso()
-					+ " " + s;
+			s = " " + mob.doc().getExNivelAcessoAtual().getNmNivelAcesso() + " " + s;
 
 			Map<ExPapel, List<Object>> mapa = mob.doc().getPerfis();
 			boolean isInteressado = false;
@@ -88,28 +86,23 @@ public class DocSiglaAssinarComSenhaPost implements
 			for (ExPapel exPapel : mapa.keySet()) {
 				Iterator<Object> it = mapa.get(exPapel).iterator();
 
-				if ((exPapel != null)
-						&& (exPapel.getIdPapel() == exPapel.PAPEL_INTERESSADO)) {
+				if ((exPapel != null) && (exPapel.getIdPapel() == exPapel.PAPEL_INTERESSADO)) {
 					while (it.hasNext() && !isInteressado) {
 						Object item = it.next();
-						isInteressado = item.toString().equals(
-								titular.getSigla()) ? true : false;
+						isInteressado = item.toString().equals(titular.getSigla()) ? true : false;
 					}
 				}
 
 			}
 
 			if (mob.doc().isSemEfeito()) {
-				if (!mob.doc().getCadastrante().equals(titular)
-						&& !mob.doc().getSubscritor().equals(titular)
+				if (!mob.doc().getCadastrante().equals(titular) && !mob.doc().getSubscritor().equals(titular)
 						&& !isInteressado) {
-					throw new AplicacaoException("Documento " + mob.getSigla()
-							+ " cancelado ");
+					throw new AplicacaoException("Documento " + mob.getSigla() + " cancelado ");
 				}
 			} else {
-				throw new AplicacaoException("Documento " + mob.getSigla()
-						+ " inacessível ao usuário " + titular.getSigla() + "/"
-						+ lotaTitular.getSiglaCompleta() + "." + s);
+				throw new AplicacaoException("Documento " + mob.getSigla() + " inacessível ao usuário "
+						+ titular.getSigla() + "/" + lotaTitular.getSiglaCompleta() + "." + s);
 			}
 		}
 	}
@@ -123,16 +116,14 @@ public class DocSiglaAssinarComSenhaPost implements
 	 * This TypeAdapter unproxies Hibernate proxied objects, and serializes them
 	 * through the registered (or default) TypeAdapter of the base class.
 	 */
-	private static class HibernateProxyTypeAdapter extends
-			TypeAdapter<HibernateProxy> {
+	private static class HibernateProxyTypeAdapter extends TypeAdapter<HibernateProxy> {
 
 		public static final TypeAdapterFactory FACTORY = new TypeAdapterFactory() {
 			@Override
 			@SuppressWarnings("unchecked")
 			public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
-				return (HibernateProxy.class
-						.isAssignableFrom(type.getRawType()) ? (TypeAdapter<T>) new HibernateProxyTypeAdapter(
-						gson) : null);
+				return (HibernateProxy.class.isAssignableFrom(type.getRawType())
+						? (TypeAdapter<T>) new HibernateProxyTypeAdapter(gson) : null);
 			}
 		};
 		private final Gson context;
@@ -148,8 +139,7 @@ public class DocSiglaAssinarComSenhaPost implements
 
 		@SuppressWarnings({ "rawtypes", "unchecked" })
 		@Override
-		public void write(JsonWriter out, HibernateProxy value)
-				throws IOException {
+		public void write(JsonWriter out, HibernateProxy value) throws IOException {
 			if (value == null) {
 				out.nullValue();
 				return;
@@ -160,8 +150,7 @@ public class DocSiglaAssinarComSenhaPost implements
 			// serialization
 			TypeAdapter delegate = context.getAdapter(TypeToken.get(baseType));
 			// Get a filled instance of the original class
-			Object unproxiedValue = ((HibernateProxy) value)
-					.getHibernateLazyInitializer().getImplementation();
+			Object unproxiedValue = ((HibernateProxy) value).getHibernateLazyInitializer().getImplementation();
 			// Serialize the value
 			// delegate.write(out, unproxiedValue);
 			delegate.write(out, "__OMITIDO__");
